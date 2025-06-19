3e0e9defdc43fa5e97736d4bbfb5d7fbb7e52ae5
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-06-18 21:59:43 -0300

    fixed return of functions

--- lib/poly_hok/type_inference.ex
+++ lib/poly_hok/type_inference.ex
@@ -86,6 +86,7 @@ defmodule PolyHok.TypeInference do
   end
   defp check_return([h|t]) do
     IO.inspect "AHHUUUUUUUU"
+    IO.inspect t
     [h|check_return t]
   end
   defp check_return([com]) do
