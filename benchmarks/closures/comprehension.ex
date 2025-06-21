100f34eeec86f24fc2d6c6cd8a5e115d5c3956b9
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-06-21 09:43:13 -0300

    debuging closures

--- benchmarks/closures/comprehension.ex
+++ benchmarks/closures/comprehension.ex
@@ -4,7 +4,9 @@ use Comp
 
 dev_vet = PolyHok.new_gnx(Nx.tensor([1,2,3,4,5,6,7,8,9,10]))
 
-resp = Comp.gpu_for n <- dev_vet,  do: n * n
+v = 3
+
+resp = Comp.gpu_for n <- dev_vet,  do: v * n
 resp
 |> PolyHok.get_gnx
 |> IO.inspect
