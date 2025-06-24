0d509f7eeb7891e54c78be3b630c99a6b442296b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-06-24 10:03:26 -0300

    debuging closures

--- lib/poly_hok/ske.ex
+++ lib/poly_hok/ske.ex
@@ -86,6 +86,7 @@ PolyHok.defmodule Ske do
   end
 
   @defaults %{coord: false, return: true, dim: :one}
+  def map(a,b,c,options \\[])
   def map({:nx, type, shape, name , ref}, func, [par1,par2], options )do
     %{coord: coord, return: return, dim: dim} = Enum.into(options, @defaults)
   case dim do
