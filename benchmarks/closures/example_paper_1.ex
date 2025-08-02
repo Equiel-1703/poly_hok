87e9e8e388540e00e0f5e218d76ec9f4c0d2d37a
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-08-02 10:14:06 -0300

    debuging closures

--- benchmarks/closures/example_paper_1.ex
+++ benchmarks/closures/example_paper_1.ex
@@ -2,14 +2,14 @@ require PolyHok
 use Ske
 
 
-dev_vet = PolyHok.new_gnx(Nx.tensor(Enum.to_list(1..1000), type: :s32))
+dev_arr = PolyHok.new_gnx(Nx.tensor(Enum.to_list(1..1000), type: :s32))
 
 x = 10
 
 fun = PolyHok.clo fn y -> x + y end 
 
-host_vet = dev_vet
+host_arr = dev_arr
 |> Ske.map(fun)
 |> PolyHok.get_gnx
 
-IO.inspect host_vet
+IO.inspect host_arr
