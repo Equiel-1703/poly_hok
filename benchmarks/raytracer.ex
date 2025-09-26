14852b101bc93931dff4e9ba104327704afa7a5c
ardubois <ardubois@github.com>
2025-09-26 12:22:51 -0300

    ray

--- benchmarks/raytracer.ex
+++ benchmarks/raytracer.ex
@@ -86,8 +86,8 @@ defk mapxy_2D_step_2_para_no_resp_kernel(d_array,  step, par1, par2,size,f) do
   end
 end
 def mapxy_2D_para_no_resp(d_array,  step,par1, par2, size, f) do
-
-    PolyHok.spawn(&RayTracer.mapxy_2D_step_2_para_no_resp_kernel/6,{trunc(size/16),trunc(size/16),1},{16,16,1},[d_array,step,par1,par2,size,f])
+  PolyHok.spawn(&RayTracer.mapxy_2D_step_2_para_no_resp_kernel/6,{size,size,1},{1,1,1},[d_array,step,par1,par2,size,f])
+   # PolyHok.spawn(&RayTracer.mapxy_2D_step_2_para_no_resp_kernel/6,{trunc(size/16),trunc(size/16),1},{16,16,1},[d_array,step,par1,par2,size,f])
     d_array
 end
 
