a477acd52f644153dbb8669064990b9d040e5c83
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-09-30 11:26:27 -0300

    mm closures

--- benchmarks/closures/mm.ex
+++ benchmarks/closures/mm.ex
@@ -9,12 +9,13 @@ m = String.to_integer(arg)
 mat1 = Nx.tensor(Enum.to_list(1..(m*m)), type: :f32)
 mat2 = Nx.tensor(Enum.to_list(1..(m*m)),  type: :f32)
 
-prev = System.monotonic_time()
-
-mat1 = PolyHok.new_gnx(Nx.reshape(mat1,{m,m}))
-mat2 = PolyHok.new_gnx(Nx.reshape(mat2,{m,m}))
+mat1 = Nx.reshape(mat1,{m,m})
+mat2 = Nx.reshape(mat2,{m,m})
 
 prev = System.monotonic_time()
+
+mat1 = PolyHok.new_gnx(mat1)
+mat2 = PolyHok.new_gnx(mat2)
 
 
 
