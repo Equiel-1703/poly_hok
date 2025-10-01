c5733ba5d280eb2254ff56495aeecd2f162e5b5b
Andre Rauber Du Bois <rauberdubois@gmail.com>
2025-10-01 13:48:24 -0300

    saxpy closures

--- benchmarks/closures/saxpy.ex
+++ benchmarks/closures/saxpy.ex
@@ -7,8 +7,8 @@ size = String.to_integer(arg)
 #host_a = Enum.to_list(1..size) |> Nx.tensor(type: :f64)
 #host_b = Enum.to_list(1..size) |> Nx.tensor(type: :f64)
 
-host_a = PolyHok.new_nx_from_function(1,size,{:f,64},fn -> :rand.uniform(1000) end)
-host_b = PolyHok.new_nx_from_function(1,size,{:f,64},fn -> :rand.uniform(1000) end)
+host_a = PolyHok.new_nx_from_function(1,size,{:f,64},fn -> 1.0 end) #:rand.uniform(1000) end)
+host_b = PolyHok.new_nx_from_function(1,size,{:f,64},fn -> 1.0 end) #:rand.uniform(1000) end)
 prev = System.monotonic_time()
 
 a = PolyHok.new_gnx(host_a)
